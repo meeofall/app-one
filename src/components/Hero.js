5e1f2b0e998370bb92785afac73eaaf602375f6a
Michael <re4maxion@gmail.com>
2021-06-20 23:43:03 +0100

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,7 +13,7 @@ function Hero() {
             <div class="container container-fluid m-3 mt-5 pb-3 pt-5 bg-white display4">
               <h1 class="display-4">Powering Schools</h1>
                   <p class="lead">with simple, yet detailed <span class="easy"><i>'easy-to-use</i>'</span> management modules.</p>
-                  <a class="btn btn-primary btn-lg" href="https://beta-class.com/trial" target="_blank" role="button">Get Started for free</a>
+                  <a class="btn btn-primary btn-lg" href="https://ischoolmanager.com/trial" target="_blank" role="button">Get Started for free</a>
                 </div>
                 
 
@@ -68,7 +68,7 @@ function Hero() {
             <a class="btn btn-lg btn-success btn-text hero-button text-white" href="https://clients.inovateos.com/signup"><i class="fa fa-home" width="20" height="20"></i> <small>Create School</small>
             </a>
             
-            <a class="btn btn-lg btn-danger btn-text hero-button text-white" href="https://beta-class.com/sch/demo"><small>Access Demo</small>
+            <a class="btn btn-lg btn-danger btn-text hero-button text-white" href="https://ischoolmanager.com/sch/demo"><small>Access Demo</small>
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-right-fill text-white" viewBox="0 0 16 16">
   <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
               </svg>              
